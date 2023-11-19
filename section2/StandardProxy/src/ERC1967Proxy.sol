921647b849068975a50a5114e1b4fc18226dc64a
calvinchang <q20274982@gmail.com>
2023-11-19 21:49:40 +0800

    class: 1026 proxy

--- section2/StandardProxy/src/ERC1967Proxy.sol
+++ section2/StandardProxy/src/ERC1967Proxy.sol
@@ -6,19 +6,35 @@ import { Proxy } from "./utils/Proxy.sol";
 
 contract ERC1967Proxy is Slots, Proxy {
 
+  bytes32 constant IMPLEMENTATION_SLOT = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
+  bytes32 constant ADMIN_SLOT = bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1);
+  
   constructor(address _impl, bytes memory _data) {
     // TODO:
     // 1. set the implementation address at bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+    _setSlotToAddress(IMPLEMENTATION_SLOT, _impl);
     // 2. set admin owner address at bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
+    _setSlotToAddress(ADMIN_SLOT, msg.sender);
     // 3. if data exist, then initialize proxy with _data
+    if (_data.length > 0) {
+      (bool success,) = _impl.delegatecall(_data);
+      require(success, "init fail");
+    }
+  }
+
+  function bytes32ToAddress(bytes32 _bytes32) internal pure returns (address) {
+    return address(uint160(uint256(_bytes32)));
   }
 
   function implementation() public view returns (address impl) {
     // TODO: return the implementation address
+    return bytes32ToAddress(IMPLEMENTATION_SLOT);
   }
 
   modifier onlyAdmin {
     // TODO: check if msg.sender is equal to admin owner address
+    address _admin = _getSlotToAddress(ADMIN_SLOT);
+    require(msg.sender == _admin, "ERC1967Proxy: admin only");
     _;
   }
 
@@ -26,6 +42,11 @@ contract ERC1967Proxy is Slots, Proxy {
     // TODO:
     // 1. upgrade the implementation address
     // 2. initialize proxy, if data exist, then initialize proxy with _data
+    _setSlotToAddress(IMPLEMENTATION_SLOT, newImplementation);
+    if (_data.length > 0) {
+      (bool success,) = newImplementation.delegatecall(_data);
+      require(success);
+    }
   }
 
   fallback() external payable virtual {
--- section2/StandardProxy/src/Transparent.sol
+++ section2/StandardProxy/src/Transparent.sol
@@ -17,12 +17,19 @@ contract Transparent is Slots, Proxy {
   constructor(address _implementation, bytes memory data) {
     // TODO:
     // 1. set the implementation address at bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+    _setSlotToAddress(IMPLEMENTATION_SLOT, _implementation);
     // 2. set admin owner address at bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
+    _setSlotToAddress(ADMIN_SLOT, msg.sender);
     // 3. if data exist, then initialize proxy with _data
+    if (data.length > 0) {
+      (bool success) = _implementation.delegatecall(data);
+      require(success, "Transparent: init failed");
+    }
   }
 
   function _getAdmin() internal view returns (address) {
     // TODO: return the admin owner address
+    return _getSlotToAddress(ADMIN_SLOT);
   }
 
   function _upgradeToAndCall(address newImplementation, bytes memory data) internal {
@@ -36,6 +43,13 @@ contract Transparent is Slots, Proxy {
   fallback() external payable {
     // TODO:
     // 1. check if msg.sender is equal to admin owner address, if no then delegatecall to implementation address
+    if (msg.sender == _getAdmin()) {
+      require(msg.sig == ITransparentUpgradeableProxy.upgradeToAndCall.selector, "Transparent: admin could only upgradeAndCall");
+      (address impl, bytes memory data) = abi.decode(msg.data[4:], (address, bytes));
+      _upgradeToAndCall(impl, data);
+    } else {
+      _delegate(implementation());
+    }
     // 2. if yes, then check if function selector is equal to upgradeToAndCall, if no then revert with Message "Transparent: admin could only upgradeAndCall"
     // 3. if yes, upgrade the implementation address and initialize proxy with data
   }
