23ef8a46f3678e0f465a729ed5b61a05147370d4
calvinchang <q20274982@gmail.com>
2023-11-26 23:13:46 +0800

    class 11

--- section3/FlashSwapPractice/contracts/Liquidator.sol
+++ section3/FlashSwapPractice/contracts/Liquidator.sol
@@ -45,13 +45,43 @@ contract Liquidator is IUniswapV2Callee, Ownable {
     //
 
     function uniswapV2Call(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external override {
-        // TODO
+        require(sender == address(this), "Sender must be this contract");
+        require(amount0 > 0 || amount1 > 0, "One of the amount must be greater 0");
+
+        // 4. decode callback data
+        CallbackData memory callbackData = abi.decode(data, (CallbackData));
+
+        // 5. call liquidate
+        IERC20(callbackData.borrowToken).approve(_FAKE_LENDING_PROTOCOL, callbackData.borrowAmount);
+        IFakeLendingProtocol(_FAKE_LENDING_PROTOCOL).liquidatePosition();
+
+        // 6. deposit ETH to WETH9, because we will get WETH9 from lending pool
+        IWETH(callbackData.repayToken).deposit{ value: callbackData.repayAmount }();
+
+        // 7. swap WETH9 to repay token
+        IERC20(callbackData.repayToken).transfer(msg.sender, callbackData.repayAmount);
     }
 
     // we use single hop path for testing
     function liquidate(address[] calldata path, uint256 amountOut) external {
         require(amountOut > 0, "AmountOut must be greater than 0");
         // TODO
+        // 1. get unitswap pool address
+        address pair = IUniswapV2Factory(_UNISWAP_FACTORY).getPair(path[0], path[1]);
+
+        // 2. calculate repay amount
+        uint256[] memory amountIn = IUniswapV2Router01(_UNISWAP_ROUTER).getAmountsIn(amountOut, path);
+
+        CallbackData memory callbackData = CallbackData(path[0], path[1], amountIn[0], amountOut);
+        // 3. flash swap from uniswap pool
+        IUniswapV2Pair(pair).swap(0, amountOut, address(this), abi.encode(callbackData));
+    }
+
+    struct CallbackData {
+        address repayToken;
+        address borrowToken;
+        uint256 repayAmount;
+        uint256 borrowAmount;
     }
 
     receive() external payable {}
